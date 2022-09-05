d9577f73586fd14be99ac086548bef21a7a95945
Bobby Cloutier <bobby.cloutier@gmail.com>
2022-09-05 12:39:43 -0400

    Adding a new objects processor.

---
+++ objects.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+class Area:
+    
+    # what goes into the area
+    # - items
+    # - exits
+    # - others
+    
+    def get_area:
+        return 1
+    
+    
+    def check_user_data():
+        return 1
+        
+class Items:
+    
+    def get_item:
+        return 1
