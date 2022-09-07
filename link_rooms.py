763f74fa211c413a21026c038f1478ecb865466d
Bobby <bobby.cloutier@gmail.com>
2022-09-06 21:23:32 -0400

    Added idea for linking rooms

---
+++ link_rooms.py
@@ -0,0 +1,27 @@
+areas =  [
+    {
+    "id" : 1,
+    "exits" : [
+        {
+        "id" : 1,
+        "type" : "door",
+        "connects_to" : (2,1)
+        }
+    ]
+    },
+    {
+    "id" : 2,
+    "exits" : [
+        {
+        "id" : 1,
+        "type" : "door"
+        },
+        {
+        "id" : 2,
+        "type" : "window",
+        "connects_to" : (1,1)
+        }
+    ]
+    }
+
+]
