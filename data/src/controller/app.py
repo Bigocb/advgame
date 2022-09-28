44b89e8a38310e1f97eca8db4e328f3caddfd7ef
bobby.cloutier <bobby.cloutier@creditkarma.com>
2022-09-28 16:40:32 -0400

    Updating URLs

--- data/src/controller/app.py
+++ data/src/controller/app.py
@@ -103,4 +103,4 @@ def update_entry():
     return {'url': 'test', 'status': 200}
 
 port = int(os.environ.get('PORT', 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
+app.run(debug=True, host='0.0.0.0', port=port,ssl_context='adhoc')
